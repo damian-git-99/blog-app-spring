864b2c299fb2bd5915e86c28a8b2b0cddcba3eda
damian <damiangalvan66@gmail.com>
2023-08-06 12:17:23 -0600

    feat(config): add PasswordConfig class for configuring password encoder feat(config): add UserService class for implementing UserDetailsService feat(config): add AuthenticationFilter class for handling authentication requests feat(config): update SpringSecurityConfig class to use UserDetailsService and PasswordEncoder

--- blog-backend/src/main/java/com/blog/app/config/SpringSecurityConfig.java
+++ blog-backend/src/main/java/com/blog/app/config/SpringSecurityConfig.java
@@ -1,26 +1,61 @@
 package com.blog.app.config;
 
+import com.blog.app.config.filters.AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
 
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Autowired
+    public SpringSecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(
+            HttpSecurity http,
+            @Autowired AuthenticationManager authenticationManager
+    ) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/api/1.0/auth").permitAll()
+                .requestMatchers("/api/**").hasRole("USER")
+                .requestMatchers("/secure/info").authenticated()
+                .anyRequest().permitAll();
+        http.addFilter(new AuthenticationFilter(authenticationManager));
+        //http.addFilterBefore(new TokenAuthenticationFilter(), AuthenticationFilter.class);
+        return http.build();
+    }
+
     @Bean
     public AuthenticationManager authManager(HttpSecurity http) throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER")
-                .and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder)
                 .and()
                 .build();
     }
+
+
 
 }
---
+++ blog-backend/src/main/java/com/blog/app/config/filters/AuthenticationFilter.java
@@ -0,0 +1,86 @@
+package com.blog.app.config.filters;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+        setRequiresAuthenticationRequestMatcher(
+                new AntPathRequestMatcher("/api/1.0/auth",
+                        HttpMethod.POST.toString())
+        );
+    }
+
+    @SneakyThrows
+    @Override
+    public Authentication attemptAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response) throws AuthenticationException {
+
+        String email = "";
+        String password = "";
+
+        try {
+            Map body = new ObjectMapper().readValue(request.getInputStream(), Map.class);
+            email = body.get("email").toString();
+            password = body.get("password").toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request
+            , HttpServletResponse response
+            , FilterChain chain
+            , Authentication authResult) throws IOException, ServletException {
+
+        Cookie cookie = new Cookie("token", "dasdsadasdasd");
+        cookie.setSecure(true);
+        response.addCookie(cookie);
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("email", "damian@gmail.com");
+        body.put("username", "damian");
+        body.put("id", "3123123dasdasdasdsada");
+
+        response.setContentType("application/json");
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request
+            , HttpServletResponse response
+            , AuthenticationException failed) throws IOException, ServletException {
+        Map<String, Object> body = new HashMap<>();
+        body.put("error", "Authentication ERROR: incorrect username or password");
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+    }
+}
