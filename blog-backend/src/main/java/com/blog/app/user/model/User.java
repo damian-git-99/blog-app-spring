208b006ce6e621f44cc345333c5e3bc9c96fe5ba
damian <damiangalvan66@gmail.com>
2023-08-10 11:35:05 -0600

    feat(User.java): add validation annotations to the User model fields for improved data integrity and error handling

--- blog-backend/src/main/java/com/blog/app/user/model/User.java
+++ blog-backend/src/main/java/com/blog/app/user/model/User.java
@@ -1,5 +1,7 @@
 package com.blog.app.user.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class User {
     private Long id;
+    @NotEmpty(message = "Username cannot be empty")
     private String username;
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Email should be valid")
     private String email;
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
 }
