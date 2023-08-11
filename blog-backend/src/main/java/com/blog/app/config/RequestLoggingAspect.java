27f4bcd1684257d4540fac0c5bf0332cf42dc23b
damian <damiangalvan66@gmail.com>
2023-08-11 09:41:32 -0600

    chore(pom.xml): add spring-aspects dependency to enable request logging aspect feat(RequestLoggingAspect.java): add aspect to log incoming requests and their URLs for better debugging and monitoring

---
+++ blog-backend/src/main/java/com/blog/app/config/RequestLoggingAspect.java
@@ -0,0 +1,23 @@
+package com.blog.app.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Slf4j
+@Aspect
+@Component
+public class RequestLoggingAspect {
+
+    @Before("execution(* com.blog.app..*Controller.*(..))")
+    public void logRequest() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String url = request.getRequestURL().toString();
+        log.info("Request received for URL: {}", url);
+    }
+
+}
