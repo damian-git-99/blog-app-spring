07d2911f42ef407181c14891e00a706eb9ea29b7
damian <damiangalvan66@gmail.com>
2023-08-12 16:40:44 -0600

    chore(UserDao): add missing methods for user management feat(UserDaoJDBC): add logging for SQL queries to improve debugging feat(UserDaoJDBC): implement findUserById method to retrieve user by id feat(UserDaoJDBC): implement editUser method to update user information feat(UserDaoJDBC): implement findUserByUsername method to retrieve user by username

--- blog-backend/src/main/java/com/blog/app/user/dao/UserDao.java
+++ blog-backend/src/main/java/com/blog/app/user/dao/UserDao.java
@@ -2,9 +2,17 @@ package com.blog.app.user.dao;
 
 import com.blog.app.user.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserDao {
     Optional<User> findUserByEmail(String email);
     boolean saveUser(User user);
+    Optional<User> findUserById(Long id);
+    boolean editUser(User user);
+    Optional<User> findUserByUsername(String username);
+    // todo: addFavoritePost
+    // todo: removeFavoritePost
+    // todo: isPostFavorite
+    // todo: getFavoritePosts
 }
--- blog-backend/src/main/java/com/blog/app/user/dao/UserDaoJDBC.java
+++ blog-backend/src/main/java/com/blog/app/user/dao/UserDaoJDBC.java
@@ -1,6 +1,7 @@
 package com.blog.app.user.dao;
 
 import com.blog.app.user.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
+@Slf4j
 public class UserDaoJDBC implements UserDao {
 
     private final JdbcTemplate jdbc;
@@ -20,7 +22,12 @@ public class UserDaoJDBC implements UserDao {
     public Optional<User> findUserByEmail(String email) {
         try {
             String query = "SELECT * FROM users WHERE email = ?";
-            User user = jdbc.queryForObject(query, BeanPropertyRowMapper.newInstance(User.class), email);
+            log.info("Executing SQL query: {}", query);
+            User user = jdbc.queryForObject(
+                    query,
+                    BeanPropertyRowMapper.newInstance(User.class),
+                    email
+            );
             return Optional.ofNullable(user);
         } catch (Exception e) {
             return Optional.empty();
@@ -30,7 +37,59 @@ public class UserDaoJDBC implements UserDao {
     @Override
     public boolean saveUser(User user) {
         String query = "INSERT INTO users (email, password, username) VALUES (?, ?, ?)";
-        int result = jdbc.update(query, user.getEmail(), user.getPassword(), user.getUsername());
+        log.info("Executing SQL query: {}", query);
+        int result = jdbc.update(
+                query,
+                user.getEmail(),
+                user.getPassword(),
+                user.getUsername()
+        );
         return result == 1;
+    }
+
+    @Override
+    public Optional<User> findUserById(Long id) {
+        String query = "SELECT * FROM users WHERE id = ?";
+        log.info("Executing SQL query: {}", query);
+        try {
+            User user = jdbc.queryForObject(
+                    query,
+                    BeanPropertyRowMapper.newInstance(User.class),
+                    id
+            );
+            return Optional.ofNullable(user);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public boolean editUser(User user) {
+        String query = "UPDATE users SET email = ?, password = ?, username = ? WHERE id = ?";
+        log.info("Executing SQL query: {}", query);
+        int res = jdbc.update(
+                query,
+                user.getEmail(),
+                user.getPassword(),
+                user.getUsername(),
+                user.getId()
+        );
+        return res == 1;
+    }
+
+    @Override
+    public Optional<User> findUserByUsername(String username) {
+        String query = "SELECT * FROM users WHERE username = ?";
+        log.info("Executing SQL query: {}", query);
+        try {
+            User user = jdbc.queryForObject(
+                    query,
+                    BeanPropertyRowMapper.newInstance(User.class),
+                    username
+            );
+            return Optional.ofNullable(user);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
 }
