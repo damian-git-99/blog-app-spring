1d7ba02f08b4368e7864c8a69a062a8c9cc24a11
damian <damiangalvan66@gmail.com>
2023-08-16 15:38:47 -0600

    refactor(JWTAuthenticationFilter.java): extract token extraction logic into a separate method for better readability and reusability fix(JWTAuthenticationFilter.java): handle case when no token is found in the request cookies to prevent NullPointerException

--- blog-backend/src/main/java/com/blog/app/config/security/filters/JWTAuthenticationFilter.java
+++ blog-backend/src/main/java/com/blog/app/config/security/filters/JWTAuthenticationFilter.java
@@ -31,33 +31,35 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
             FilterChain chain
     ) throws ServletException, IOException {
 
-        Cookie[] cookies = request.getCookies();
-        log.info("Checking token in cookies");
+        Optional<String> optionalToken = extractTokenFromRequest(request);
 
-        if (cookies != null) {
-            var optionalCookie = Arrays.stream(cookies)
-                    .filter(cookie -> cookie.getName().equals("token"))
-                    .findFirst();
-
-            if (optionalCookie.isPresent()) {
-                String token = optionalCookie.get().getValue();
-                log.info("token found: {}", token);
-                Cookie cookie = optionalCookie.get();
-                JWTAuthentication jwtAuthentication = new JWTAuthentication(token);
-                Authentication auth = authenticationManager.authenticate(jwtAuthentication);
-                if (auth.isAuthenticated()) {
-                    log.info("Authentication successful by token");
-                    SecurityContextHolder.getContext().setAuthentication(auth);
-                } else {
-                    log.info("Clearing Spring Security Context");
-                    SecurityContextHolder.clearContext();
-                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                }
+        if (optionalToken.isPresent()) {
+            String token = optionalToken.get();
+            log.info("token found: {}", token);
+            JWTAuthentication jwtAuthentication = new JWTAuthentication(token);
+            Authentication auth = authenticationManager.authenticate(jwtAuthentication);
+            if (auth.isAuthenticated()) {
+                log.info("Authentication successful by token");
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } else {
+                log.info("Clearing Spring Security Context");
+                SecurityContextHolder.clearContext();
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
             }
 
         }
 
         chain.doFilter(request, response);
+    }
+
+    private Optional<String> extractTokenFromRequest(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        log.info("Checking token in cookies");
+        if (cookies == null) return Optional.empty();
+        var optionalCookie = Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals("token"))
+                .findFirst();
+        return optionalCookie.map(Cookie::getValue);
     }
 }
