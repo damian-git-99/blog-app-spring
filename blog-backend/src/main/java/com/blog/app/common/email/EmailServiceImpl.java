ea4d8129cba820d06b19f36300d4d89936d9899e
damian <damiangalvan66@gmail.com>
2023-08-24 12:36:30 -0600

    chore(EmailServiceImpl.java): add logging to sendEmail method for better debugging and tracking of sent emails

--- blog-backend/src/main/java/com/blog/app/common/email/EmailServiceImpl.java
+++ blog-backend/src/main/java/com/blog/app/common/email/EmailServiceImpl.java
@@ -1,13 +1,15 @@
 package com.blog.app.common.email;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendEmail(String to, String subject, String body) {
-
+        log.info("Sending email to: " + to + " with subject: " + subject + " and body: " + body);
     }
 
 }
