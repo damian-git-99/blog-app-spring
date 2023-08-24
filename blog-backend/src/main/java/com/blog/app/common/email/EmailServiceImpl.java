0f91ee91ec722d10f2a858d49afecbc48478c400
damian <damiangalvan66@gmail.com>
2023-08-24 11:05:47 -0600

    feat(email): add EmailService interface and EmailServiceImpl class
    The EmailService interface is added to provide a contract for sending emails. It includes a single method `sendEmail` that takes in the recipient's email address, subject, and body of the email.
    
    The EmailServiceImpl class is implemented to provide the actual implementation of the `sendEmail` method. Currently, the method is empty and does not perform any actions.

---
+++ blog-backend/src/main/java/com/blog/app/common/email/EmailServiceImpl.java
@@ -0,0 +1,13 @@
+package com.blog.app.common.email;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+
+    }
+
+}
