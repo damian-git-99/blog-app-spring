a16a6a78f537e9a586bf3adc1edf2733b775340e
damian <damiangalvan66@gmail.com>
2023-08-25 10:15:23 -0600

    fix: update EmailServiceImpl to use JavaMailSender

--- blog-backend/src/main/java/com/blog/app/common/email/EmailServiceImpl.java
+++ blog-backend/src/main/java/com/blog/app/common/email/EmailServiceImpl.java
@@ -1,15 +1,30 @@
 package com.blog.app.common.email;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class EmailServiceImpl implements EmailService {
 
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
     @Override
     public void sendEmail(String to, String subject, String body) {
         log.info("Sending email to: " + to + " with subject: " + subject + " and body: " + body);
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(to);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(body);
+        mailSender.send(mailMessage);
     }
 
 }
