cffcee95ee561f1826b8cc0d41f99d20595f9934
damian <damiangalvan66@gmail.com>
2023-08-25 16:10:44 -0600

    refactor: improve logging

--- blog-backend/src/main/java/com/blog/app/common/image/UploadImageCloudinaryService.java
+++ blog-backend/src/main/java/com/blog/app/common/image/UploadImageCloudinaryService.java
@@ -11,18 +11,19 @@ public class UploadImageCloudinaryService implements ImageService {
     @Override
     public String uploadImage(MultipartFile file) {
         log.info("Uploading image to cloudinary");
-        System.out.println("Uploading image to cloudinary");
+        log.debug("File name: " + file.getOriginalFilename());
         return "image.png";
     }
 
     @Override
     public void deleteImage(String imageId) {
         log.info("Deleting image from cloudinary");
+        log.debug("Image id: " + imageId);
     }
 
     @Override
     public String getImageUrl(String imageId) {
-        System.out.println("Getting image url from cloudinary");
+        log.debug("Getting image url from cloudinary {} ", imageId);
         return "https://images.unsplash.com/photo-1684577753340-de97c66fa6fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80";
     }
 
