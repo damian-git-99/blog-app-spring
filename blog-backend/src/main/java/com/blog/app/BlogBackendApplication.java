c034ed0acd8180ac087fbf4a1542c9f9f893f754
damian <damiangalvan66@gmail.com>
2023-09-08 15:08:15 -0600

    refactor: Timezone set to UTC

--- blog-backend/src/main/java/com/blog/app/BlogBackendApplication.java
+++ blog-backend/src/main/java/com/blog/app/BlogBackendApplication.java
@@ -1,13 +1,22 @@
 package com.blog.app;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BlogBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogBackendApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 }
